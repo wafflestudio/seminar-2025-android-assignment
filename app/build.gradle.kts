efa554684af70e66d3fbd42a0eec356e14f53368
rlacjfals110 <rlacjfals110@snu.ac.kr>
2025-09-29 16:39:37 +0900

    김철민 과제1 제출합니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
 }
